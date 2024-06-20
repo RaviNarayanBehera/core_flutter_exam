4f66d3e164c0403508631012f49b492f6d62b037
RAVINARAYAN <ravinarayan.b9510@gmail.com>
2024-06-20 14:02:29 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:core_flutter_exam/Screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/' : (context) => const HomePage(),
+
+      },
+    );
+  }
+}
